06f89f4c8b087358267daab903db7c9cd29e5df5
vitaZ-dev <vitaminzdev@gmail.com>
2023-05-09 16:43:37 +0900

    feat: footer 영역 작업 시작
    footer 영역 구조 및 레이아웃 작업

--- js/header.js
+++ js/header.js
@@ -83,7 +83,6 @@ menuItems.forEach((item) => {
 const menuSelected = document.querySelectorAll(".sidenav-gnb-list > li > a");
 menuSelected.forEach((select) => {
   select.addEventListener("click", () => {
-    console.log(select);
     let icon = select.querySelector(".icon-select");
     icon.classList.toggle("icon-checked");
   });
